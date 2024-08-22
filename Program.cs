fa717814b75ca49ab31bcc4352999cb894c2a15f
terri-howze <47995570+terri-howze@users.noreply.github.com>
2024-08-22 14:23:34 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Numerics;
+namespace numberguessinggame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        var rand = new Random();
+        int answer = rand.Next(1,11);
+        int guessAnswer;
+        bool correctAnswer = false;
+
+
+        Console.WriteLine("Welcome to the number guessing game!\nA Number between 1 and 10 will be generated.");
+        Console.WriteLine("If you guess the correct number, you win!\nPlease enter your guess");
+ 
+        guessAnswer = Int32.Parse(Console.ReadLine());
+            while (!correctAnswer)
+            {
+                if(guessAnswer > answer)
+                {
+                    Console.WriteLine("Your guess is too high!\nPlease enter your guess.");
+                    guessAnswer = Int32.Parse(Console.ReadLine());
+                }
+                else if(guessAnswer < answer)
+                {
+                    Console.WriteLine("Your guess is too low!\nPlease enter your guess.");
+                    guessAnswer = Int32.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    Console.WriteLine("Correct!\nCongratulations, you have won the game!");
+                    correctAnswer = true;
+                }
+            }
+
+        }
+    }
+}
